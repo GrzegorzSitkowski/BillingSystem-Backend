a77080e984c447451adfe180d3ba343817ddd9d1
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-31 19:09:31 +0200

    Created entity of User

---
+++ BillingSystem.Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using BillingSystem.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingSystem.Domain.Entities
+{
+    public class User : DomainEntity
+    {
+        public required string Email { get; set; }
+        public required string HashedPassword { get; set; }
+        public DateTimeOffset RegisterDate { get; set; }
+    }
+}
