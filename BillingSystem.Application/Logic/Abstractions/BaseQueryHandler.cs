2058bfa9fc74f8f9c27460dec37769cf995b3db7
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-04-01 17:49:50 +0200

    Added and implementation BaseQueryHandler

---
+++ BillingSystem.Application/Logic/Abstractions/BaseQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using BillingSystem.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingSystem.Application.Logic.Abstractions
+{
+    public abstract class BaseQueryHandler
+    {
+        protected readonly ICurrentAccountProvider _currentAccountProvider;
+        protected readonly IApplicationDbContext _applicationDbContext;
+
+        protected BaseQueryHandler(ICurrentAccountProvider currentAccountProvider, IApplicationDbContext applicationDbContext)
+        {
+            _currentAccountProvider = currentAccountProvider;
+            _applicationDbContext = applicationDbContext;
+        }
+    }
+}
