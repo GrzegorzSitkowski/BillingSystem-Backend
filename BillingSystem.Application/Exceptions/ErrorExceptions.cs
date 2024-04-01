2a24df4602f382aa44b50bb06e87c4aed8366bea
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-04-01 18:00:20 +0200

    Created ErrorExceptions.cs

---
+++ BillingSystem.Application/Exceptions/ErrorExceptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingSystem.Application.Exceptions
+{
+    public class ErrorExceptions
+    {
+        public string Error { get; private set; }
+
+        public ErrorExceptions(string error) 
+        {
+            Error = error;
+        }
+
+    }
+}
