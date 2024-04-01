bf9fc7c435899f86eb5604769acf772e8f3f7365
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-04-01 16:29:45 +0200

    Implementation MainDbContext

---
+++ BillingSystem.Infrastructure/Persistence/MainDbContext.cs
@@ -0,0 +1,30 @@
+﻿using BillingSystem.Application.Interfaces;
+using BillingSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingSystem.Infrastructure.Persistence
+{
+    public class MainDbContext : DbContext, IApplicationDbContext
+    {
+        public MainDbContext(DbContextOptions<MainDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AccountUser> AccountUsers { get; set; }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)//metoda ustawiająca globalne ustawienia dla property
+        {
+            configurationBuilder.Properties<decimal>().HavePrecision(18, 4);
+
+            base.ConfigureConventions(configurationBuilder);
+        }
+    }
+}
