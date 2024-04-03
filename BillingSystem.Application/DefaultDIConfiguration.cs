c23ef0bf1d7d254c75dede6dddecc4a76d979979
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-04-03 18:41:58 +0200

    Added DefaultDIConfiguration.cs

---
+++ BillingSystem.Application/DefaultDIConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using BillingSystem.Application.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingSystem.Application
+{
+    public static  class DefaultDIConfiguration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICurrentAccountProvider, ICurrentAccountProvider>();
+
+            return services;
+        } 
+    }
+}
