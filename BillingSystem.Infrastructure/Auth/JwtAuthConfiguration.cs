84381196115cd08a018e21bd115dae356ef08ba5
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-04-04 18:01:48 +0200

    Added JwtAuthenticationOptions in DI

---
+++ BillingSystem.Infrastructure/Auth/JwtAuthConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using BillingSystem.Application.Interfaces;
+using BillingSystem.Infrastructure.Auth;
+using EFCoreSecondLevelCacheInterceptor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingSystem.Infrastructure.Persistence
+{
+    public static class JwtAuthConfiguration
+    {
+        public static IServiceCollection AddJwtAuth(this IServiceCollection services, IConfiguration configuration) 
+        {
+            services.Configure<JwtAuthenticationOptions>(configuration.GetSection("JwtAuthentication"));
+
+            return services;
+        }
+    }
+}
