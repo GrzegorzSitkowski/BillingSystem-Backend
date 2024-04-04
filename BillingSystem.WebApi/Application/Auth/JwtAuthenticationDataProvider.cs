99067167586a77bdb480ca7bc0b754425208faee
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-04-04 18:52:01 +0200

    Created JwtAuthenticationDataProvider.cs

---
+++ BillingSystem.WebApi/Application/Auth/JwtAuthenticationDataProvider.cs
@@ -0,0 +1,22 @@
+﻿using BillingSystem.Application.Interfaces;
+using BillingSystem.Infrastructure.Auth;
+
+namespace BillingSystem.WebApi.Application.Auth
+{
+    public class JwtAuthenticationDataProvider : IAuthenticationDataProvider
+    {
+        private readonly JwtManager _jwtManager;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public JwtAuthenticationDataProvider(JwtManager jwtManager, IHttpContextAccessor httpContextAccessor)
+        {
+            _jwtManager = jwtManager;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public int? GetUserId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
