f5c15c8b1563900a35f2fbc9c6213817ad8d83eb
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-04-04 17:56:00 +0200

    Added JwtAuthenticationOptions.cs

---
+++ BillingSystem.Infrastructure/Auth/JwtAuthenticationOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingSystem.Infrastructure.Auth
+{
+    public class JwtAuthenticationOptions
+    {
+        public string? Secret { get; set; } //hasło do szyfrowania
+        public string? Issuer { get; set; } // dostawca tokena
+        public string? Audience { get; set; } //odbiorca tokena
+        public int ExpireInDays { get; set; } = 30;
+    }
+}
