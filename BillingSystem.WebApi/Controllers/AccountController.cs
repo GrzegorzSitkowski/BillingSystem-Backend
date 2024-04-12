349e121540410f461fe12b3429568040b3b32e92
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-04-12 17:31:06 +0200

    Created AccountController

---
+++ BillingSystem.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Core;
+using BillingSystem.Application.Logic.User;
+using BillingSystem.Infrastructure.Auth;
+using BillingSystem.WebApi.Application.Auth;
+using BillingSystem.WebApi.Application.Response;
+using MediatR;
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace BillingSystem.WebApi.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class AccountController : BaseController
+    {
+        public AccountController(ILogger<AccountController> logger,
+            IMediator mediator) : base(logger, mediator)
+        {
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetCurrentAccount()
+        {
+            var data = await _mediator.Send(new CurrentAccountQuery.Request() { });
+            return Ok(data);
+        }
+    }
+}
