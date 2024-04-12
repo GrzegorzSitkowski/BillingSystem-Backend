ae08d86e6be762bf0a4a9b450c4912eb635e3166
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-04-12 16:16:39 +0200

    Added CookieSettings in UserController

--- BillingSystem.WebApi/Controllers/UserController.cs
+++ BillingSystem.WebApi/Controllers/UserController.cs
@@ -1,7 +1,10 @@
 ﻿using BillingSystem.Application.Logic.User;
+using BillingSystem.Infrastructure.Auth;
+using BillingSystem.WebApi.Application.Auth;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace BillingSystem.WebApi.Controllers
 {
@@ -9,8 +12,16 @@ namespace BillingSystem.WebApi.Controllers
     [ApiController]
     public class UserController : BaseController
     {
-        public UserController(ILogger<UserController> logger, IMediator mediator) : base(logger, mediator)
+        private readonly CookieSettings? _cookieSettings;
+        private readonly JwtManager _jwtManager;
+
+        public UserController(ILogger<UserController> logger,
+            IOptions<CookieSettings> cookieSettings,
+            JwtManager jwtManager,
+            IMediator mediator) : base(logger, mediator)
         {
+            _cookieSettings = cookieSettings != null ? cookieSettings.Value : null;
+            _jwtManager = jwtManager;
         }
 
         [HttpPost]
@@ -18,6 +29,30 @@ namespace BillingSystem.WebApi.Controllers
         {
             var createAccountResult = await _mediator.Send(model);
             return Ok(createAccountResult);
+        }
+
+        private void SetTokenCookie(string token)
+        {
+            var cookieOption = new CookieOptions()
+            {
+                HttpOnly = true,
+                Secure = true,
+                Expires = DateTime.Now.AddDays(30),
+                SameSite = SameSiteMode.Lax,
+            };
+
+            if(_cookieSettings != null)
+            {
+                cookieOption = new CookieOptions()
+                {
+                    HttpOnly = cookieOption.HttpOnly,
+                    Expires = cookieOption.Expires,
+                    Secure = _cookieSettings.Secure,
+                    SameSite = _cookieSettings.SameSite,
+                };
+            }
+
+            Response.Cookies.Append(CookieSettings.CookieName, token, cookieOption);
         }
     }
 }
