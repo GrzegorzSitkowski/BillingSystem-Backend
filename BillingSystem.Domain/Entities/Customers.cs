844acdf03071b0a5844a166dbbebca6c090ec008
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-06-04 18:51:04 +0200

    Created entity of Customers

---
+++ BillingSystem.Domain/Entities/Customers.cs
@@ -0,0 +1,20 @@
+﻿using BillingSystem.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingSystem.Domain.Entities
+{
+    public class Customers : DomainEntity
+    {
+        public DateTimeOffset CreateDate { get; set; } = DateTimeOffset.Now;
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public float PayRate { get; set; }
+        public float Balance { get; set; }
+        //public List<Invoice> Invoices { get; set; }
+    }
+}
