8cef277d50735c3234b4339f838d5016bc85a2e3
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-06-10 18:32:45 +0200

    Created entity of Customers in database. Updated ICollection in entity

--- BillingSystem.Domain/Entities/Customer.cs
+++ BillingSystem.Domain/Entities/Customer.cs
@@ -11,13 +11,13 @@ namespace BillingSystem.Domain.Entities
     {
         public DateTimeOffset CreateDate { get; set; } = DateTimeOffset.Now;
         public string FullName { get; set; }
-        public string PhoneNumner { get; set; }
+        public string PhoneNumber { get; set; }
         public string Addres { get; set; }
         public string PostCode { get; set; }
         public string City { get; set; }
         public string Email { get; set; }
         public float PayRate { get; set; }
         public float Balance { get; set; }
-        public List<Invoice> Invoices { get; set; }
+        public ICollection<Invoice> Invoices { get; set; } = new List<Invoice>();
     }
 }
