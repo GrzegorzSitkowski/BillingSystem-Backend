04869ebe3347102a080810da8c871c71b5e28fd7
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-06-11 20:04:29 +0200

    Created CustomerController.cs

---
+++ BillingSystem.WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Core;
+using BillingSystem.Application.Logic.User;
+using BillingSystem.Infrastructure.Auth;
+using BillingSystem.WebApi.Application.Auth;
+using BillingSystem.WebApi.Application.Response;
+using MediatR;
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace BillingSystem.WebApi.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class CustomerController : BaseController
+    {
+        public CustomerController(ILogger<CustomerController> logger,
+            IMediator mediator) : base(logger, mediator)
+        {
+
+        }
+    }
+}
