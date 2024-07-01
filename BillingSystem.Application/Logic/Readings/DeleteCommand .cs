9d84723b02c13d1b50b915dab9febf0032abde23
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-07-01 18:59:43 +0200

    Created command - DeleteCommand

---
+++ BillingSystem.Application/Logic/Readings/DeleteCommand .cs
@@ -0,0 +1,53 @@
+﻿using BillingSystem.Application.Exceptions;
+using BillingSystem.Application.Interfaces;
+using BillingSystem.Application.Logic.Abstractions;
+using BillingSystem.Domain.Entities;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingSystem.Application.Logic.Readings
+{
+    public static class DeleteCommand
+    {
+        public class Request : IRequest<Result>
+        {
+            public int? Id { get; set; }
+        }
+
+        public class Result
+        {
+        }
+
+        public class Handler : BaseCommandHandler, IRequestHandler<Request, Result>
+        {
+            
+            public Handler(ICurrentAccountProvider currentAccountProvider, IApplicationDbContext applicationDbContext) : base(currentAccountProvider, applicationDbContext)
+            {               
+            }
+
+            public async Task<Result> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var account = await _currentAccountProvider.GetAuthenticatedAccount();
+
+                var model = await _applicationDbContext.Readings.FirstOrDefaultAsync(c => c.Id == request.Id && c.CreatedBy == account.Id);
+
+                if(model == null)
+                {
+                    throw new UnauthorizedException();
+                }
+
+                _applicationDbContext.Readings.Remove(model);
+
+                await _applicationDbContext.SaveChangesAsync(cancellationToken);
+
+                return new Result();
+            }
+        }
+    }
+}
