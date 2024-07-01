aaa0683654158f2dbf66a62e5fe6e5c1ecb8225d
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-07-01 19:06:36 +0200

    Fixed namespaces

--- BillingSystem.Application/Logic/Customers/ListQuery.cs
+++ BillingSystem.Application/Logic/Customers/ListQuery.cs
@@ -11,7 +11,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BillingSystem.Application.Logic.Customer
+namespace BillingSystem.Application.Logic.Customers
 {
     public static class ListQuery
     {
