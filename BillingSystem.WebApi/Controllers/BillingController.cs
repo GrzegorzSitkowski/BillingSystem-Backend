f52b1861b040272ce28d61efe13ad0115bbae465
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-08-10 15:35:33 +0200

    Created BillingController.cs

---
+++ BillingSystem.WebApi/Controllers/BillingController.cs
@@ -0,0 +1,24 @@
+﻿using BillingSystem.Application.Logic.Billing;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BillingSystem.WebApi.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class BillingController : BaseController
+    {
+        public BillingController(ILogger<BillingController> logger,
+            IMediator mediator) : base(logger, mediator)
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> List([FromQuery] ListQuery.Request model)
+        {
+            var data = await _mediator.Send(model);
+            return Ok(data);
+        }
+    }
+}
