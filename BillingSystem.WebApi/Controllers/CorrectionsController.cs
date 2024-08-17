3fb6f7710197eb5141f312cb070b66c7cfb8cf0c
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-08-17 13:51:06 +0200

    Implemenation CorrectionsController

---
+++ BillingSystem.WebApi/Controllers/CorrectionsController.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using BillingSystem.Application.Logic.Invoices;
+
+namespace BillingSystem.WebApi.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class CorrectionsController : BaseController
+    {
+        public CorrectionsController(ILogger<CorrectionsController> logger,
+            IMediator mediator) : base(logger, mediator)
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([FromBody] CreateCommand.Request model)
+        {
+            var data = await _mediator.Send(model);
+            return Ok(data);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Delete([FromBody] DeleteCommand.Request model)
+        {
+            var data = await _mediator.Send(model);
+            return Ok(data);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetCorrection([FromQuery] GetQuery.Request model)
+        {
+            var data = await _mediator.Send(model);
+            return Ok(data);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> List([FromQuery] ListQuery.Request model)
+        {
+            var data = await _mediator.Send(model);
+            return Ok(data);
+        }
+    }
+}
