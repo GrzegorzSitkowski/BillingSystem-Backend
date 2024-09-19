14f9030aee90b6f1e07797ef980a0ff62a79b16c
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-09-19 18:39:08 +0200

    Implementation Print.cs

--- BillingSystem.Application/Logic/Invoices/Print.cs
+++ BillingSystem.Application/Logic/Invoices/Print.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BillingSystem.Application.Interfaces;
+using BillingSystem.Application.Logic.Abstractions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,40 @@ using System.Threading.Tasks;
 
 namespace BillingSystem.Application.Logic.Invoices
 {
-    public class Print
+    public static class Print
     {
+        public class Request
+        {
+            public int? Id { get; set; }
+        }
+
+        public class Handler : BaseQueryHandler
+        {
+            public Handler(ICurrentAccountProvider currentAccountProvider, IApplicationDbContext applicationDbContext) : base(currentAccountProvider, applicationDbContext)
+            {
+            }
+
+            public async Task Handle(Request request,  CancellationToken cancellationToken)
+            {
+                var account = await _currentAccountProvider.GetAuthenticatedAccount();
+
+                var model = await _applicationDbContext.Invoices.FirstOrDefaultAsync(c => c.Id == request.Id && c.CreatedBy == account.Id);
+
+                if(model == null)
+                {
+                    throw new UnauthorizedAccessException();
+                }
+
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(path, "invoice.txt"), true))
+                {
+                    outputFile.WriteLine($"Number of customer: {model.CustomerId}");
+                    outputFile.WriteLine($"Name: {model.CustomerName}");
+                    outputFile.WriteLine($"Amount: {model.Amount}");
+                    outputFile.WriteLine($"Due date: {model.DueDate}");
+                }
+            }
+        }
     }
 }
