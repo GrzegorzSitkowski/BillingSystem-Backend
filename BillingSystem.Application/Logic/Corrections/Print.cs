521ef85a64c5b2828f18b8e726a58cf50272d503
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-09-22 20:27:51 +0200

    Added Print.cs in Corrections

---
+++ BillingSystem.Application/Logic/Corrections/Print.cs
@@ -0,0 +1,65 @@
+﻿using BillingSystem.Application.Interfaces;
+using BillingSystem.Application.Logic.Abstractions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BillingSystem.Application.Logic.Corrections
+{
+    public static class Print
+    {
+        public class Request : IRequest<Result>
+        {
+            public int? Id { get; set; }
+        }
+
+        public class Result
+        {
+            public int Id { get; set; }
+        }
+
+        public class Handler : BaseQueryHandler, IRequestHandler<Request, Result>
+        {
+            public Handler(ICurrentAccountProvider currentAccountProvider, IApplicationDbContext applicationDbContext) : base(currentAccountProvider, applicationDbContext)
+            {
+            }
+
+            public async Task<Result> Handle(Request request,  CancellationToken cancellationToken)
+            {
+                var account = await _currentAccountProvider.GetAuthenticatedAccount();
+
+                var model = await _applicationDbContext.Corrections.FirstOrDefaultAsync(c => c.Id == request.Id && c.CreatedBy == account.Id);
+                var customer = await _applicationDbContext.Customers.FirstOrDefaultAsync(c => c.Id == model.CustomerId);
+
+                if(model == null)
+                {
+                    throw new UnauthorizedAccessException();
+                }
+
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(path, "correction.txt"), true))
+                {
+                    outputFile.WriteLine($"Number of customer: {model.CustomerId}");
+                    outputFile.WriteLine('\n');
+                    outputFile.WriteLine($"{customer.FullName}");
+                    outputFile.WriteLine($"{customer.Address}");
+                    outputFile.WriteLine($"{customer.PostCode}");
+                    outputFile.WriteLine($"{customer.City}");
+                    outputFile.WriteLine($"{customer.Email}");
+                    outputFile.WriteLine('\n');
+  
+                    outputFile.WriteLine($"Value of invoice: {model.Amount} zł");
+                    outputFile.WriteLine($"Reason: {model.Reason}");
+                    outputFile.WriteLine($"Describe: {model.Describe}");
+                    outputFile.WriteLine($"Your balance: {customer.Balance}");
+                    outputFile.WriteLine('\n');
+                }
+
+                return new Result()
+                {
+                    Id = model.Id
+                };          
+            }
+        }
+    }
+}
