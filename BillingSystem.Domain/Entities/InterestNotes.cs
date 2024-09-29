c12061f846cc23e4d45c82b8f66f4eb8df2301e7
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-09-29 15:48:12 +0200

    Created entity od InterestNotes

---
+++ BillingSystem.Domain/Entities/InterestNotes.cs
@@ -0,0 +1,21 @@
+﻿using BillingSystem.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingSystem.Domain.Entities
+{
+    public class InterestNotes : DomainEntity
+    {
+        public int InvoiceId { get; set; }
+        public int CustomerId { get; set; }
+        public double Amount { get; set; }
+        public string DocumentNumber { get; set; }
+        public string InvoiceNumber { get; set; }
+        public DateTimeOffset DueDate { get; set; }
+        public DateTimeOffset CreateDate { get; set; }
+        public int CreatedBy { get; set; }
+    }
+}
