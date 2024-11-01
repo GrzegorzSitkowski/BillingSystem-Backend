81106d10075136b187398fbcb6de084842ece2a5
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-11-01 15:21:30 +0100

    Created entity of payment

---
+++ BillingSystem.Domain/Entities/Payment.cs
@@ -0,0 +1,20 @@
+﻿using BillingSystem.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingSystem.Domain.Entities
+{
+    public class Payment : DomainEntity
+    {
+        public int CustometId { get; set; }
+        public int DocumentId { get; set; }
+        public string DocumentNumber { get; set; }
+        public double Amount { get; set; }
+        public DateTimeOffset CreateDate { get; set; } = DateTimeOffset.Now;
+        public Customer Customer { get; set; } = default!;
+        public Invoice Invoice { get; set; } = default!;
+    }
+}
